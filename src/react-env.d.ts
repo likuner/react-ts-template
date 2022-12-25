52134518171b414f95762c9f7d73e9b93031f30f
likun <273676636@qq.com>
2022-12-25 17:58:11 +0800

    Update react-env.d.ts

--- src/react-env.d.ts
+++ src/react-env.d.ts
@@ -43,17 +43,7 @@ declare module '*.css' {
   export default classes
 }
 
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
-
 declare module '*.less' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
-
-declare module '*.module.less' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
