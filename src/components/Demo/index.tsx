8d43e82af56e1e45ca4e8fe6308a7ee2edd722f3
likun <273676636@qq.com>
2022-12-25 17:54:02 +0800

    add antd

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.less'
 import logo from '../../assets/logo.png'
+import { Alert, Input, Space } from 'antd'
 
 export default function Demo() {
-  return <div className={styles.container}>
-    hello
-    <img src={logo} />
-  </div>
+  const [msg, setMsg] = useState('Welcome to React Template!')
+
+  const handleChange = (e: any) => {
+    setMsg(e.target.value)
+  }
+
+  return (
+    <Space direction='vertical' size='middle' className={styles.container}>
+      <img src={logo} className={styles.imgBox} />
+      <Alert type='info' message={msg} />
+      <Input value={msg} onChange={handleChange}/>
+    </Space>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#00b96b'
+      }
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ConfigProvider>
 )
