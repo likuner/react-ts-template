c69342186c3fcdc7b545675fbf4901cb447e6067
likun <273676636@qq.com>
2022-12-27 11:01:09 +0800

    add custom hooks

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Button, Result } from 'antd'
+import { Button, Result, Space, Typography } from 'antd'
+import { useWeek } from '../../hooks/useWeek'
+
+const { Text } = Typography
 
 const About: React.FC = () => {
   const { title = '🥤这是 About 组件' } = useParams()
+
+  const week = useWeek()
 
   const navigate = useNavigate()
 
@@ -11,7 +16,10 @@ const About: React.FC = () => {
     <Result
       title={title}
       extra={
-        <Button type="primary" onClick={() => navigate('/')}>Back Home</Button>
+        <Space size="middle" direction="vertical">
+          <Text type="warning">{week}</Text>
+          <Button type="primary" onClick={() => navigate('/')}>Back Home</Button>
+        </Space>
       }
     />
   )
---
+++ src/hooks/useWeek.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+export const useWeek = (time: number | Date = new Date()): string => {
+  const [week, setWeek] = useState('')
+  
+  useEffect(() => {
+    const weekArr: string[] = ['日', '一', '二', '三', '四', '五', '六']
+    if (typeof time === 'number') {
+      setWeek(weekArr[new Date(time).getDay()])
+    } else {
+      setWeek(weekArr[time.getDay()])
+    }
+  }, [time])
+
+  return `星期${week}`
+}
